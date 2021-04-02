76c3865f4d4070395e0ed1583aa978d393967a11
Danil Lans <danimen0001@gmail.com>
2021-04-02 09:45:19 +0300

    lab 1 all done

--- InformationSecurity/InformationSecurity/StatisticalQualityPlaintexts.cpp
+++ InformationSecurity/InformationSecurity/StatisticalQualityPlaintexts.cpp
@@ -63,12 +63,47 @@ void writeToFile(string fileName, string text)
 
 string dataEncryption(string text, size_t columns, const vector<int> keys) 
 {
-	
+	string result{ "" };
+	string resultDataString{ "" };
+	size_t zCount = columns - text.size() % columns;
+	size_t index{ 0 };
+
+	for (size_t i = 0; i < text.size(); ++i)
+	{
+		result += text[i];
+	}
+
+	for (size_t i = 0; i < zCount; ++i)
+	{
+		result.append("z");
+	}
+
+	for (auto i : keys)
+	{
+		index = 0;
+		while (i + index * columns - 1 < result.size())
+		{
+			resultDataString += result[i - 1 + index++ * columns];
+		}
+	}
+	return resultDataString;
 }
 
-string dataDecryption()
+string dataDecryption(string text, size_t columns, const vector<int> keys )
 {
+	size_t rows = text.size() / columns;
+	size_t indexVector{ 0 };
+	string decryptionData{ "" };
 
+	for (size_t i = 0; i < rows; i++)
+	{
+		for (size_t j = 0; j < columns; j++)
+		{
+			indexVector = find(keys.begin(), keys.end(), j + 1 )- keys.begin();
+			decryptionData += text[i + (indexVector) * rows];
+		}
+	}
+	return decryptionData;
 }
 
 int main()
@@ -87,9 +122,12 @@ int main()
 	string keyFileName = "key.txt";
 	string phraseFileName = "phrase.txt";
 	string writeEncryptionFileName = "code.txt";
-	string writeReEncryptionFileName = "decode.txt";
+	string writeDecryptionFileName = "decode.txt";
 	string dataFromKeyFile = readFromFile(keyFileName);
 	string dataFromPhraseFile = readFromFile(phraseFileName);
+
+	string encryptionData{ "" };
+	string decryptionData{ "" };
 
 	stringstream iss(dataFromKeyFile);
 	int number;
@@ -98,6 +136,13 @@ int main()
 		keys.push_back(number);
 
 	cout << "key: " << dataFromKeyFile << endl;
+
+	encryptionData = dataEncryption(dataFromPhraseFile, columns, keys);
+	writeToFile(writeEncryptionFileName, encryptionData);
+	
+	decryptionData = dataDecryption(encryptionData, columns, keys);
+	writeToFile(writeDecryptionFileName, decryptionData);
+
 
 
 
