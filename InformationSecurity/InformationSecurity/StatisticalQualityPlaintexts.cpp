19c193caac986b44f33e21bc85c21071b5824d83
Danil Lans <danimen0001@gmail.com>
2021-04-10 16:41:27 +0300

    add docx, change readFile

--- InformationSecurity/InformationSecurity/StatisticalQualityPlaintexts.cpp
+++ InformationSecurity/InformationSecurity/StatisticalQualityPlaintexts.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>;
-#include <string_view>;
 #include <map>;
 #include <vector>;
 #include <sstream>;
@@ -16,19 +15,22 @@ string readFromFile(string fileName)
 
 	if (fin.is_open())
 	{
-		while (!fin.eof())
-		{
-			getline(fin, outStringLine);
-			resultOutString += outStringLine;
-		}
+		// get length of file:
+		fin.seekg(0, fin.end);
+		int length = fin.tellg();
+		fin.seekg(0, fin.beg);
+
+		resultOutString.resize(length);
+		fin.read(&resultOutString[0], length);
 		fin.close();
 	}
 	else
 	{
-		cout << "Problem with file :(\n";
+		cout << "Problem with file :( \n";
 	}
 	return resultOutString;
 }
+
 
 void print_map(const map<char, size_t>& m)
 {
@@ -42,7 +44,7 @@ map<char, size_t> getFrequencyMap(string text)
 {
 	map<char, size_t> result;
 	stringstream inputStringStream(text);
-	
+
 	char word;
 	while (inputStringStream >> word)
 		result[word]++;
@@ -61,12 +63,14 @@ void writeToFile(string fileName, string text)
 	out.close();
 }
 
-string dataEncryption(string text, size_t columns, const vector<int> keys) 
+string dataEncryption(string text, size_t columns, const vector<int> keys, size_t* zAmount)
 {
 	string result{ "" };
 	string resultDataString{ "" };
 	size_t zCount = columns - text.size() % columns;
 	size_t index{ 0 };
+	
+	*zAmount = zCount;
 
 	for (size_t i = 0; i < text.size(); ++i)
 	{
@@ -75,8 +79,9 @@ string dataEncryption(string text, size_t columns, const vector<int> keys)
 
 	for (size_t i = 0; i < zCount; ++i)
 	{
-		result.append("z");
+		result.append("z");	
 	}
+	
 
 	for (auto i : keys)
 	{
@@ -89,7 +94,7 @@ string dataEncryption(string text, size_t columns, const vector<int> keys)
 	return resultDataString;
 }
 
-string dataDecryption(string text, size_t columns, const vector<int> keys )
+string dataDecryption(string text, size_t columns, const vector<int> keys, size_t zAmount)
 {
 	size_t rows = text.size() / columns;
 	size_t indexVector{ 0 };
@@ -99,10 +104,17 @@ string dataDecryption(string text, size_t columns, const vector<int> keys )
 	{
 		for (size_t j = 0; j < columns; j++)
 		{
-			indexVector = find(keys.begin(), keys.end(), j + 1 )- keys.begin();
-			decryptionData += text[i + (indexVector) * rows];
+			indexVector = find(keys.begin(), keys.end(), j + 1) - keys.begin();
+			decryptionData += text[i + (indexVector)* rows];
 		}
 	}
+	
+	
+	for (size_t i = 0; i < zAmount; i++)
+	{
+		decryptionData.pop_back();
+	}
+	
 	return decryptionData;
 }
 
@@ -111,6 +123,7 @@ int main()
 	// 1
 	string fileName = "Veni.docx";
 	string textFromFile = readFromFile(fileName);
+
 	cout << "Size of file: " << textFromFile.size() << endl;
 
 	// 2
@@ -119,7 +132,10 @@ int main()
 
 	// 3
 	const size_t columns = 5;
-	string keyFileName = "key.txt";
+
+	//txt
+	/*
+		string keyFileName = "key.txt";
 	string phraseFileName = "phrase.txt";
 	string writeEncryptionFileName = "code.txt";
 	string writeDecryptionFileName = "decode.txt";
@@ -137,15 +153,46 @@ int main()
 
 	cout << "key: " << dataFromKeyFile << endl;
 
-	encryptionData = dataEncryption(dataFromPhraseFile, columns, keys);
-	writeToFile(writeEncryptionFileName, encryptionData);
+	size_t var = 0;
 	
-	decryptionData = dataDecryption(encryptionData, columns, keys);
+	
+	encryptionData = dataEncryption(dataFromPhraseFile, columns, keys, &var);
+	writeToFile(writeEncryptionFileName, encryptionData);
+
+	decryptionData = dataDecryption(encryptionData, columns, keys, var);
 	writeToFile(writeDecryptionFileName, decryptionData);
+	cout << "  " << var << endl;
+
+	*/
+
+	//docx
+	string keyFileName = "key.txt";
+	string phraseFileName = "phrase.docx";
+	string writeEncryptionFileName = "code.docx";
+	string writeDecryptionFileName = "decode.docx";
+	string dataFromKeyFile = readFromFile(keyFileName);
+	string dataFromPhraseFile = readFromFile(phraseFileName);
+
+	string encryptionData{ "" };
+	string decryptionData{ "" };
+
+	stringstream iss(dataFromKeyFile);
+	int number;
+	vector <int> keys;
+	while (iss >> number)
+		keys.push_back(number);
+
+	cout << "key: " << dataFromKeyFile << endl;
+
+	size_t var = 0;
 
 
+	encryptionData = dataEncryption(dataFromPhraseFile, columns, keys, &var);
+	writeToFile(writeEncryptionFileName, encryptionData);
 
-
+	decryptionData = dataDecryption(encryptionData, columns, keys, var);
+	writeToFile(writeDecryptionFileName, decryptionData);
+	cout << "  " << var << endl;
 	system("pause");
 	return 0;
 }
